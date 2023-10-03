d219429235bac6e71083179a70aebc6fe9abb31e
Sahith Aitha <aithasahith0214@gmail.com>
2023-10-03 19:40:55 +0530

    Added Styles.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -30,29 +30,29 @@ function Keyboard() {
     }
     return (
         <div>
-            <h1>{errorMessage}</h1>
+            <h1 className='Error'>{errorMessage}</h1>
             <Screen inputText={inputText} result={result}></Screen>
             <div className='buttonsContainer'>
                 <button onClick={clickHandler} name="%">%</button>
-                <button onClick={reset}>Reset</button>
+                <button onClick={reset}>AC</button>
                 <button onClick={backSpace} name="X">X</button>
-                <button onClick={clickHandler} name="/">/</button>
+                <button className="operatorsButton" onClick={clickHandler} name="/">/</button>
                 <button onClick={clickHandler} name="7">7</button>
                 <button onClick={clickHandler} name="8">8</button>
                 <button onClick={clickHandler} name="9">9</button>
-                <button onClick={clickHandler} name="*">*</button>
+                <button className="operatorsButton"  onClick={clickHandler} name="*">*</button>
                 <button onClick={clickHandler} name="4">4</button>
                 <button onClick={clickHandler} name="5">5</button>
                 <button onClick={clickHandler} name="6">6</button>
-                <button onClick={clickHandler} name="-">-</button>
+                <button className="operatorsButton"  onClick={clickHandler} name="-">-</button>
                 <button onClick={clickHandler} name="1">1</button>
                 <button onClick={clickHandler} name="2">2</button>
                 <button onClick={clickHandler} name="3">3</button>
-                <button onClick={clickHandler} name="+">+</button>
+                <button className="operatorsButton"  onClick={clickHandler} name="+">+</button>
                 <button onClick={clickHandler} name="00">00</button>
                 <button onClick={clickHandler} name="0">0</button>
                 <button onClick={clickHandler} name=".">.</button>
-                <button onClick={calculateHandler} name="">=</button>
+                <button className="equalsButton"onClick={calculateHandler} name="">=</button>
             </div>
         </div>
     )
